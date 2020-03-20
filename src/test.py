aee5091380d7db2abd744e5e2cb44f48bfb47c31
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-03-20 23:20:35 +0900

    Add natural to corrupitons

--- src/test.py
+++ src/test.py
@@ -8,10 +8,10 @@ import tqdm
 
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision import transforms
+from torchvision import transforms, datasets
 from tqdm import tqdm
 
-from utils import load_txt, accuracy, create_barplot, get_fname
+from utils import load_txt, accuracy, create_barplot, get_fname, AverageMeter
 from models.resnet import ResNet56
 from dataset import CIFAR10C
 
@@ -31,15 +31,30 @@ def main(opt):
     model.to(device)
     model.eval()
 
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.49139968, 0.48215841, 0.44653091],
+                             [0.24703223, 0.24348513, 0.26158784],)
+    ])
+
     accs = dict()
     with tqdm(total=len(opt.corruptions), ncols=80) as pbar:
         for ci, cname in enumerate(opt.corruptions):
-            dataset = CIFAR10C(opt.data_root, cname,
-                                transform=transforms.ToTensor())
+            # load dataset
+            if cname == 'natural':
+                dataset = datasets.CIFAR10(
+                    os.path.join(opt.data_root, 'cifar10'),
+                    train=False, transform=transform
+                )
+            else:
+                dataset = CIFAR10C(
+                    os.path.join(opt.data_root, 'cifar10-c'),
+                    cname, transform=transform
+                )
             loader = DataLoader(dataset, batch_size=opt.batch_size,
                                 shuffle=False, num_workers=4)
             
-            acc_sum = 0
+            acc_meter = AverageMeter()
             with torch.no_grad():
                 for itr, (x, y) in enumerate(loader):
                     x = x.to(device, non_blocking=True)
@@ -49,12 +64,11 @@ def main(opt):
                     z = model(x)
                     loss = F.cross_entropy(z, y)
                     acc, _ = accuracy(z, y, topk=(1, 5))
-                    acc_sum += acc.item()
+                    acc_meter.update(acc.item())
 
-            acc_avg = acc_sum / (itr+1)
-            accs[f'{cname}'] = acc_avg
+            accs[f'{cname}'] = acc_meter.avg
 
-            pbar.set_postfix_str(f'{cname}: {acc_avg:.2f}')
+            pbar.set_postfix_str(f'{cname}: {acc_meter.avg:.2f}')
             pbar.update()
     
     pprint.pprint(accs)
@@ -79,7 +93,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--data_root',
-        type=str, default='/home/tanimu/data/cifar10-c',
+        type=str, default='/home/tanimu/data',
         help='root path to cifar10-c directory'
     )
     parser.add_argument(
